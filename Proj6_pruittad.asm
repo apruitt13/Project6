9b559637cb91a682e1e2d6317dba33225c698fda
apruitt13 <pruittad@oregonstate.edu>
2022-03-04 06:44:51 -0800

    Add project files.

---
+++ Proj6_pruittad.asm
@@ -0,0 +1,30 @@
+TITLE Project 6     (Proj6_pruittad.asm)
+
+; Author: Adam Pruitt
+; Last Modified: 3/13/22
+; OSU email address: pruittad@oregonstate.edu
+; Course number/section:   CS271 Section 401
+; Project Number:     6            Due Date: 3/13/22
+; Description: 
+
+INCLUDE Irvine32.inc
+
+; (insert macro definitions here)
+
+; (insert constant definitions here)
+
+.data
+
+; (insert variable definitions here)
+
+.code
+main PROC
+
+; (insert executable instructions here)
+
+	Invoke ExitProcess,0	; exit to operating system
+main ENDP
+
+; (insert additional procedures here)
+
+END main
